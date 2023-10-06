a202bba29c88347e12bfcf7585f4a49f37d0ee36
Legaljay <adesinaayobami67@gmail.com>
2023-10-06 12:11:43 +0100

    added a button with state isOpen that toggles the couter

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,11 @@ const messages = [
 ];
 function App() {
    const [step, setStep] = useState(1)
+   const [isOpen, setIsOpen] = useState(true)
 
+   function toggle(){
+    setIsOpen(!isOpen);
+   }
   function handlePrevious(){
     setStep((prev) => prev == 1? 1: prev - 1)
   }
@@ -18,22 +22,26 @@ function App() {
     setStep((prev) => prev >= 3 ? 3: prev + 1)
   }
   return (
-    <div className='steps'>
-      <div className='numbers'>
-        <div className={step >= 1 ? 'active': ''}>1</div>
-        <div className={step >= 2 ? 'active': ''}>2</div>
-        <div className={step >= 3 ? 'active': ''}>3</div>
-      </div>
+    <div>
+      <button className="close" onClick={toggle}>&times;</button>
+      {isOpen && (<div className='steps'>
+        <div className='numbers'>
+          <div className={step >= 1 ? 'active': ''}>1</div>
+          <div className={step >= 2 ? 'active': ''}>2</div>
+          <div className={step >= 3 ? 'active': ''}>3</div>
+        </div>
 
-      <p className='message'>Step {step}: {messages[step - 1]}</p>
+        <p className='message'>Step {step}: {messages[step - 1]}</p>
 
-      <div className="buttons">
-        <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handlePrevious}>Previous</button>
-        <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handleNext}>Next</button>
-      </div>
+        <div className="buttons">
+          <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handlePrevious}>Previous</button>
+          <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handleNext}>Next</button>
+        </div>
 
+      </div>)}
     </div>
   )
 }
 
 export default App
+ 
