8337cdd1512c373af248c0c09f6c6e15d874f94e
Legaljay <adesinaayobami67@gmail.com>
2023-10-06 13:39:32 +0100

    wrote a dynamic date counter using state

---
+++ public/components/dateCounter.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export const DateCounter = () => {
+    const [step, setStep] = useState(1)
+    const [count, setCount] = useState(0)
+
+    const date = new Date("june 21 2027")
+    date.setDate(date.getDate() + count)
+  return (
+    <div>
+        <div>
+            <button onClick={() => setStep(prev => prev - 1)}>-</button>
+            <span>Step: {step}</span>
+            <button onClick={() => setStep(prev => prev + 1)}>+</button>
+        </div>
+        <div>
+            <button onClick={() => setCount(prevCount => prevCount - step)}>-</button>
+            <span>Count: {count}</span>
+            <button onClick={() => setCount(prevCount => prevCount + step)}>+</button>
+        </div>
+        <p>{ count == 0 ? "today is " : count > 0 ? `${count} days from today is ` : `${Math.abs(count)} days ago was `}{date.toDateString()}</p>
+    </div>
+  )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css'
+import { DateCounter } from '../public/components/dateCounter';
 
 
 const messages = [
@@ -39,6 +40,7 @@ function App() {
         </div>
 
       </div>)}
+      <DateCounter />
     </>
   )
 }
